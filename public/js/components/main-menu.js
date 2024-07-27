4e6517357456ffde54dcb61f333a74cccfea14f8
theBigTank <codeplay.erniie@gmail.com>
2024-07-27 05:54:44 +0100

    styled buttons

--- public/js/components/main-menu.js
+++ public/js/components/main-menu.js
@@ -1,4 +1,12 @@
 export default function MainMenu() {
-    
+  let block = document.querySelector('header');
+
+  if (block) {
+    const hamburger = block.querySelector(".hamburger");
+      
+    hamburger.addEventListener("click", function() {
+      hamburger.classList.toggle("is-active");
+    });
+  }
 }
   
